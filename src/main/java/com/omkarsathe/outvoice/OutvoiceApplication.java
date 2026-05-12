dd2f19e92db3500062a467be3699813686e9791f
Omkar Sathe <omkar@omkarsathe.com>
2026-05-12 20:33:09 +0530

    initial commit

---
+++ src/main/java/com/omkarsathe/outvoice/OutvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.omkarsathe.outvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omkarsathe/outvoice/OutvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkarsathe.outvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
